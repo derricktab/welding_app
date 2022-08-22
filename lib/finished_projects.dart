a9e2e60c740d38e4c7255ca08b64e79f126d7226
DERRICK ZZIWA <derricktab44@gmail.com>
2022-08-22 10:23:22 +0300

    f

--- lib/finished_projects.dart
+++ lib/finished_projects.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:welding_app/constants.dart';
 
@@ -9,12 +10,60 @@ class FinishedProjects extends StatefulWidget {
 }
 
 class _FinishedProjectsState extends State<FinishedProjects> {
-  
-  var finished_projects = [];
-
-
   @override
   Widget build(BuildContext context) {
+    var imgList = [
+      "assets/images/finished1.jpeg",
+      "assets/images/finished2.jpeg",
+    ];
+
+    var finished_projects = <Map<String, dynamic>>[
+      {
+        "prodId": 01,
+        "name": "Wooden Door",
+        "price": 278000,
+        "image": "assets/images/wdoor.jpg",
+        "description": "Very durable product made from the finest materials.",
+      },
+      {
+        "prodId": 02,
+        "name": "Wooden Door",
+        "price": 190000,
+        "image": "assets/images/wdoor1.jpg",
+        "description": "Very durable product made from the finest materials.",
+      },
+      {
+        "prodId": 02,
+        "name": "Closet",
+        "price": 600000,
+        "image": "assets/images/f1.jpg",
+        "description": "Very durable product made from the finest materials.",
+      },
+    ];
+
+// IMAGE SLIDERS
+    final List<Widget> imageSliders = imgList
+        .map((item) => Container(
+              margin: EdgeInsets.all(5.0),
+              height: 600,
+              child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.asset(
+                        item,
+                        fit: BoxFit.cover,
+                        width: 1000.0,
+                        height: 400,
+                      ),
+                    ],
+                  )),
+            ))
+        .toList();
+
+    int _current = 0;
+    final CarouselController _controller = CarouselController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Finished Projects'),
@@ -38,7 +87,23 @@ class _FinishedProjectsState extends State<FinishedProjects> {
             margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             height: 250,
             decoration: BoxDecoration(
-                color: Colors.orange, borderRadius: BorderRadius.circular(20)),
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: CarouselSlider(
+              items: imageSliders,
+              carouselController: _controller,
+              options: CarouselOptions(
+                  height: 300,
+                  autoPlay: false,
+                  enlargeCenterPage: true,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }),
+            ),
           )
         ],
       ),
