ca9345d984cb180921b7efde0ab7b88c6cdbc928
DERRICK ZZIWA <derricktab44@gmail.com>
2022-08-22 22:49:51 +0300

    fd

--- lib/finished_projects.dart
+++ lib/finished_projects.dart
@@ -14,34 +14,44 @@ class FinishedProjects extends StatefulWidget {
 class _FinishedProjectsState extends State<FinishedProjects> {
   @override
   Widget build(BuildContext context) {
-    var imgList = [
+    var finished = [
+      "assets/images/finished.jpeg",
+    ];
+    var finished1 = [
+      "assets/images/finished1.jpeg",
+    ];
+    var finished2 = [
+      "assets/images/finished2.jpeg",
+      "assets/images/finished3.jpeg",
+    ];
+    var finished3 = [
       "assets/images/finished.jpeg",
       "assets/images/finished1.jpeg",
       "assets/images/finished2.jpeg"
     ];
 
-// IMAGE SLIDERS
-    final List<Widget> imageSliders = imgList
-        .map((item) => Container(
-              margin: EdgeInsets.all(5.0),
-              height: 600,
-              child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(15.0)),
-                  child: Stack(
-                    children: <Widget>[
-                      Image.asset(
-                        item,
-                        fit: BoxFit.cover,
-                        width: 1000.0,
-                        height: 400,
-                      ),
-                    ],
-                  )),
-            ))
-        .toList();
+// // IMAGE SLIDERS
+//     final List<Widget> imageSliders = imgList
+//         .map((item) => Container(
+//               margin: EdgeInsets.all(5.0),
+//               height: 600,
+//               child: ClipRRect(
+//                   borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+//                   child: Stack(
+//                     children: <Widget>[
+//                       Image.asset(
+//                         item,
+//                         fit: BoxFit.cover,
+//                         width: 1000.0,
+//                         height: 400,
+//                       ),
+//                     ],
+//                   )),
+//             ))
+//         .toList();
 
-    int _current = 0;
-    final CarouselController _controller = CarouselController();
+//     int _current = 0;
+//     final CarouselController _controller = CarouselController();
 
     return Scaffold(
       appBar: AppBar(
@@ -89,7 +99,7 @@ class _FinishedProjectsState extends State<FinishedProjects> {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: MJImageSlider(
-                images: imgList,
+                images: finished,
                 useLocalAssetImages: true,
                 duration: const Duration(seconds: 3),
                 width: 200,
@@ -106,7 +116,24 @@ class _FinishedProjectsState extends State<FinishedProjects> {
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
               child: MJImageSlider(
-                images: imgList,
+                images: finished1,
+                useLocalAssetImages: true,
+                duration: const Duration(seconds: 3),
+                width: 200,
+                height: 200,
+                curve: Curves.easeInOutCubicEmphasized,
+              ),
+            ),
+          ),
+
+          // images slider
+          Container(
+            margin: const EdgeInsets.all(22),
+            height: 270,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: MJImageSlider(
+                images: finished3,
                 useLocalAssetImages: true,
                 duration: const Duration(seconds: 3),
                 width: 200,
