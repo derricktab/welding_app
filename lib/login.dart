2ab416dd28584da064808bd0ee80ad9234caf85d
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-03 18:19:33 +0300

    j

--- lib/login.dart
+++ lib/login.dart
@@ -319,8 +319,26 @@ class _LoginState extends State<Login> {
                           print("CUSER TYPE: ${cUser.size}");
                           if (cUser.size <= 0) {
                             // Add the user to cloudfirestore
-                            
+                            var _user = {
+                              "uid": user.uid,
+                              "name": user.displayName,
+                              "email": user.email,
+                              "phone": user.phoneNumber,
+                              "address": "",
+                              "image":
+                                  "https://firebasestorage.googleapis.com/v0/b/invention-plus.appspot.com/o/userImages%2F2022-09-02%2014%3A51%3A26.244059.png?alt=media&token=8b87a755-6660-44a5-988c-56f6fe5b899d",
+                            };
+
+                            FirebaseFirestore.instance
+                                .collection("users")
+                                .doc(user.uid)
+                                .set(_user)
+                                .then((value) {
+                              print("Data added to Cloud Firestore");
+                            });
                           } else {
+                            print(
+                                "THE USER ALREADY EXISTS Size: ${cUser.size}");
                             // get the user details from firestore
                           }
 
