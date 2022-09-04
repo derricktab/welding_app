3a2d443e258f1ad8b7d4abfe1918a10c5647caa3
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-04 15:52:31 +0300

    jkfd

--- lib/addProduct.dart
+++ lib/addProduct.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import "package:flutter/material.dart";
 import 'package:welding_app/constants.dart';
+import 'package:path_provider/path_provider.dart';
+import 'models/balcony.dart';
 
 class AddProduct extends StatefulWidget {
   const AddProduct({Key? key}) : super(key: key);
@@ -13,19 +15,6 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
-  var prod = {
-    "prodId": 41,
-    "name": "Advanced New Windows",
-    "price": 1000000,
-    "image": ["assets/images/gate.jpg"],
-    "description": "Very durable product made from the finest materials.",
-  };
-
-  var prodStream = FirebaseFirestore.instance
-      .collection("products")
-      .doc("gypsum")
-      .collection("gypsum");
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,85 +24,26 @@ class _AddProductState extends State<AddProduct> {
       body: ListView(
         children: [
           ElevatedButton(
-              onPressed: () {
-// MAIN DOORS
-                var maindoors = <Map<String, dynamic>>[
-                  {
-                    "prodId": 01,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 01,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor.jpg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 02,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor1.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 03,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor2.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 04,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor3.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 04,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor4.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 04,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor5.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                  {
-                    "prodId": 04,
-                    "name": "Main Door",
-                    "price": 2000000 - 2900000,
-                    "image": "assets/images/mdoor6.jpeg",
-                    "description":
-                        "Very durable product made from the finest materials.",
-                  },
-                ];
-
-                FirebaseStorage.instance
-                    .ref()
-                    .child("products/")
-                    .putFile(File(""))
-                    .then((p0) async {
-                  print(await p0.ref.getDownloadURL());
+              onPressed: () async {
+                print(balcony.length);
+                balcony.forEach((element) async {
+                  await FirebaseFirestore.instance
+                      .collection("products")
+                      .doc("balcony")
+                      .collection("balcony")
+                      .add(element)
+                      .then(
+                    (value) {
+                      FirebaseFirestore.instance
+                          .collection("products")
+                          .doc("balcony")
+                          .collection("balcony")
+                          .doc(value.id)
+                          .update({"prodId": value.id}).then(
+                              (value) => print("Updated Prod ID"));
+                    },
+                  );
                 });
-                prodStream
-                    .add(prod)
-                    .then((value) => print("Product Added to cloud"));
               },
               child: const Text("ADD PRODUCT"))
         ],
--- lib/edit_prodile.dart
+++ lib/edit_prodile.dart
@@ -1,12 +1,9 @@
 import 'dart:io';
-import 'dart:ui';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 
 class EditProfile extends StatefulWidget {
