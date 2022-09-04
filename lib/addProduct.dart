dd57c7e6cd6a583d8de43e19909e56a6ae19a468
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-04 11:35:22 +0300

    h

--- lib/addProduct.dart
+++ lib/addProduct.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import "package:flutter/material.dart";
 import 'package:welding_app/constants.dart';
 
@@ -33,6 +36,81 @@ class _AddProductState extends State<AddProduct> {
         children: [
           ElevatedButton(
               onPressed: () {
+// MAIN DOORS
+                var maindoors = <Map<String, dynamic>>[
+                  {
+                    "prodId": 01,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 01,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor.jpg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 02,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor1.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 03,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor2.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 04,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor3.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 04,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor4.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 04,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor5.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                  {
+                    "prodId": 04,
+                    "name": "Main Door",
+                    "price": 2000000 - 2900000,
+                    "image": "assets/images/mdoor6.jpeg",
+                    "description":
+                        "Very durable product made from the finest materials.",
+                  },
+                ];
+
+                FirebaseStorage.instance
+                    .ref()
+                    .child("products/")
+                    .putFile(File(""))
+                    .then((p0) async {
+                  print(await p0.ref.getDownloadURL());
+                });
                 prodStream
                     .add(prod)
                     .then((value) => print("Product Added to cloud"));
