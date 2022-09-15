a3e5e78a9147b37fdf5de7ab18e1ebdc95886767
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-15 10:35:24 +0300

    hj

--- lib/school.dart
+++ lib/school.dart
@@ -12,28 +12,49 @@ class _SchoolState extends State<School> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryColor,
+        backgroundColor: primaryColor,
         appBar: AppBar(
           backgroundColor: primaryColor,
           elevation: 0,
         ),
         body: ListView(
-          children: const [
+          children: [
             SizedBox(
-              height: 100,
-              child: Center(
-                child: Text(
-                  "INVENTION PLUS JUNIOR SCHOOL",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "times new roman"),
-                  textAlign: TextAlign.center,
-                ),
+              height: 160,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/bts-removebg-preview.png"),
+                  const Expanded(
+                    child: Text(
+                      "INVENTION PLUS JUNIOR SCHOOL",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "times new roman"),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
               ),
             ),
-            Text("INVENTION PLUS JUNIOR SCHOOL"),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.yellow,
+                      blurRadius: 5,
+                      offset: Offset.fromDirection(2))
+                ],
+                borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(35),
+                    topLeft: Radius.circular(35)),
+                color: Colors.white,
+              ),
+              height: 700,
+              child: const Text("INVENTION PLUS JUNIOR SCHOOL"),
+            ),
           ],
         ));
   }
