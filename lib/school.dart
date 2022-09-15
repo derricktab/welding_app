33d83ed3054121e0a1b8c1f23a47ef2c0ab8b887
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-15 10:12:30 +0300

    hj

--- lib/school.dart
+++ lib/school.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:welding_app/constants.dart';
 
 class School extends StatefulWidget {
   const School({super.key});
@@ -11,8 +12,29 @@ class _SchoolState extends State<School> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: ListView(children: const [Text("INVENTION PLUS JUNIOR SCHOOL"),],)
-    );
+        appBar: AppBar(
+          backgroundColor: primaryColor,
+          elevation: 0,
+        ),
+        body: ListView(
+          children: [
+            Container(
+              color: primaryColor,
+              height: 100,
+              child: const Center(
+                child: Text(
+                  "INVENTION PLUS JUNIOR SCHOOL",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: "times new roman"),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            const Text("INVENTION PLUS JUNIOR SCHOOL"),
+          ],
+        ));
   }
 }
