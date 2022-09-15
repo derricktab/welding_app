25a15a258bbd473c9125462a5d68d9eabc201e5a
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-15 08:20:43 +0300

    hjfdsdf

---
+++ lib/forgot_password.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  var _formKey = GlobalKey<FormState>();
+  var _email = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Forgot Password"),
+      ),
+      body: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: _email,
+                decoration: const InputDecoration(hintText: "Enter Your Email"),
+              )
+            ],
+          )),
+    );
+  }
+}
