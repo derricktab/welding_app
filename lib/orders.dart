3ca35748e495f7a6f7d26fc5ede2789d45e51ca0
DERRICK ZZIWA <derricktab44@gmail.com>
2022-09-20 18:02:46 +0300

    orders

--- lib/orders.dart
+++ lib/orders.dart
@@ -9,6 +9,29 @@ class Orders extends StatefulWidget {
 }
 
 class _OrdersState extends State<Orders> {
+  var orders = <Map<String, dynamic>>[
+    {
+      "user": "43",
+      "orderId": "1",
+      "items": ["sugar", "salt", "irish"],
+      "additionalInfo": "iwant rice",
+      "orderDate": "2019"
+    },
+    {
+      "user": "43",
+      "orderId": "1",
+      "items": ["sugar", "salt", "irish"],
+      "additionalInfo": "iwant rice",
+      "orderDate": "2019"
+    },
+    {
+      "user": "43",
+      "orderId": "1",
+      "items": ["sugar", "salt", "irish"],
+      "additionalInfo": "iwant rice",
+      "orderDate": "2019"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,40 +49,61 @@ class _OrdersState extends State<Orders> {
         centerTitle: true,
         backgroundColor: primaryColor,
       ),
-      body: Column(
-        children: [
-          Image.asset("assets/images/empty.jpg"),
+      body: orders.isEmpty
+          ? Column(
+              children: [
+                Image.asset("assets/images/empty.jpg"),
 
-          const SizedBox(height: 30),
+                const SizedBox(height: 30),
 
-          const Text(
-            "YOU HAVE NOT MADE ANY ORDERS YET",
-            style: TextStyle(fontSize: 18),
-          ),
+                const Text(
+                  "YOU HAVE NOT MADE ANY ORDERS YET",
+                  style: TextStyle(fontSize: 18),
+                ),
 
-          const SizedBox(height: 50),
+                const SizedBox(height: 50),
 
-          //PLACE ORDER BUTTON
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                shape: BeveledRectangleBorder(
-                    borderRadius: BorderRadius.circular(12)),
-              ),
-              onPressed: () async {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, "services");
-              },
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0),
-                child: Text("PLACE ORDER"),
-              ),
-            ),
-          ),
-        ],
-      ),
+                //PLACE ORDER BUTTON
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.red,
+                      shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                    ),
+                    onPressed: () async {
+                      Navigator.pop(context);
+                      Navigator.pushNamed(context, "services");
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15.0),
+                      child: Text("PLACE ORDER"),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          : OrderList(orders: orders),
     );
+  }
+}
+
+class OrderList extends StatefulWidget {
+  final List orders;
+
+  const OrderList({super.key, required this.orders});
+
+  @override
+  State<OrderList> createState() => _OrderListState();
+}
+
+class _OrderListState extends State<OrderList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: ((context, index) {
+      return Container(child: const Text("This is inter"));
+    }));
   }
 }
